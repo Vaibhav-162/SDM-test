ea078f3008699ac4f05214914c438a80416250b6
vaibhav shinde <vaibhav65632@gmail.com>
2023-07-10 15:27:27 +0530

    second version

--- routes/books.js
+++ routes/books.js
@@ -24,12 +24,23 @@ bookRouter.get("/", (req, res) => {
 bookRouter.post("/", (req, res) => {
   const statement = `insert into Book_Tb (b_name, author, book_type , price, publishedDate, language) values ('${req.body.b_name}', '${req.body.author}', '${req.body.book_type}', ${req.body.price} , '${req.body.publishedDate}', '${req.body.language}');`;
   db.query(statement, (error, result) => {
-    console.log(result);
     if (error) {
       res.send("error");
     } else {
       res.setHeader("Content-Type", "application/json");
-      res.send(JSON.stringify({ "Rows affected": result.affectedRows }));
+      res.send(result.affectedRows);
+    }
+  });
+});
+
+bookRouter.put("/:id", (req, res) => {
+  const statement = `update Book_Tb set price = ${req.body.price}, language = '${req.body.language}' where id = ${req.params.id}`;
+  db.query(statement, (error, result) => {
+    if (error) {
+      res.send("error");
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.send(result.affectedRows);
     }
   });
 });
